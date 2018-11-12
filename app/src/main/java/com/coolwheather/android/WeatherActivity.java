9399cb0fbe6e936346131ccb7cd1694eba634b93
lichenxi <962707557@qq.com>
2018-11-12 11:31:26 +0800

    更改应用图标并加入更新数据服务

--- app/src/main/java/com/coolwheather/android/WeatherActivity.java
+++ app/src/main/java/com/coolwheather/android/WeatherActivity.java
@@ -8,6 +8,7 @@ import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -28,6 +29,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.coolwheather.android.gson.Forecast;
 import com.coolwheather.android.gson.Weather;
+import com.coolwheather.android.service.AutoUpdateService;
 import com.coolwheather.android.util.HttpUtil;
 import com.coolwheather.android.util.Utility;
 
@@ -116,19 +118,6 @@ public class WeatherActivity extends AppCompatActivity {
         });
     }
 
-    private void initLayout() {
-        if (Build.VERSION.SDK_INT>=21) {
-            Window window = getWindow();
-
-            //系统栏半透明，允许状态栏系统栏更改颜色
-            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
-            |WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-            window.setStatusBarColor(Color.TRANSPARENT);
-            window.setNavigationBarColor(Color.TRANSPARENT);
-        }
-    }
 
     /**
      * 根据天气ID请求城市天气信息
@@ -221,6 +210,8 @@ public class WeatherActivity extends AppCompatActivity {
         sportText.setText(sport);
 
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this,AutoUpdateService.class);
+        startService(intent);
     }
 
     /**
@@ -251,6 +242,20 @@ public class WeatherActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void initLayout() {
+        if (Build.VERSION.SDK_INT>=21) {
+            Window window = getWindow();
+
+            //系统栏半透明，允许状态栏系统栏更改颜色
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
+                    |WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setNavigationBarColor(Color.TRANSPARENT);
+        }
     }
 
     //初始化各控件
--- app/src/main/java/com/coolwheather/android/service/AutoUpdateService.java
+++ app/src/main/java/com/coolwheather/android/service/AutoUpdateService.java
@@ -1,8 +1,23 @@
 package com.coolwheather.android.service;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+
+import com.coolwheather.android.gson.Weather;
+import com.coolwheather.android.util.HttpUtil;
+import com.coolwheather.android.util.Utility;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class AutoUpdateService extends Service {
     public AutoUpdateService() {
@@ -10,7 +25,84 @@ public class AutoUpdateService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                updateWeather();
+                updateBingPic();
+            }
+        }).start();
+        AlarmManager manager= (AlarmManager) getSystemService(ALARM_SERVICE);
+        int anHour = 2*60*60*1000; //2小时
+        long triggerAtTime = SystemClock.elapsedRealtime() + anHour;
+        Intent i = new Intent(this,AutoUpdateService.class);
+        PendingIntent pi = PendingIntent.getService(this,0,i,0);
+        manager.cancel(pi);
+        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,triggerAtTime,pi);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    /**
+     * 更新天气信息
+     */
+    private void updateWeather() {
+         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String weatherString = prefs.getString("weather",null);
+        if (weatherString != null) {
+            //有缓存直接解析天气信息
+            Weather weather = Utility.handleWeatherResponse(weatherString);
+            String weatherId = weather.basic.weatherId;
+
+            String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId +
+                    "&key=bc0418b57b2d4918819d3974ac1285d9";
+            HttpUtil.sendOKHttpRequest(weatherUrl, new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    String responseText = response.body().string();
+                    Weather weather = Utility.handleWeatherResponse(responseText);
+                    if (weather != null && "ok".equals(weather.status)) {
+                        SharedPreferences.Editor editor = PreferenceManager.
+                                getDefaultSharedPreferences(AutoUpdateService.this).edit();
+                        editor.putString("weather",responseText);
+                        editor.apply();
+                    }
+
+                }
+            });
+
+
+        }
+    }
+
+    /**
+     * 更新每日一图
+     */
+    private void updateBingPic() {
+        String requestBingPic = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOKHttpRequest(requestBingPic, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseText = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager.
+                        getDefaultSharedPreferences(AutoUpdateService.this).edit();
+                editor.putString("bing_pic",responseText);
+                editor.apply();
+            }
+        });
     }
 }
